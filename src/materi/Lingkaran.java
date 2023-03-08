25a9357cabfe2b8fb84361e4d2bf0bec37701abf
MuhammadIrtada <muhammadirtada@gmail.com>
2023-03-08 18:12:44 +0700

    add method display

--- src/materi/Lingkaran.java
+++ src/materi/Lingkaran.java
@@ -1,39 +1,44 @@
 package materi;
 
 public class Lingkaran {
-    // public double jariJari;
+    //public double jariJari;
     public double jariJari = 7; // -> memberi nilai default
 
     public double getDiameter(){
-        double diameter = jariJari * 2;
-        return diameter;
-        // return jariJari * 2; // -> kalau mau cepetnya bisa gini
+        // double diameter = jariJari * 2;
+        // return diameter;
+        return jariJari * 2; // -> kalau mau cepetnya bisa gini
     }
 
     public double getKeliling(){
-        double keliling = 2 * jariJari * Math.PI;
-        // double keliling = getDiameter() * Math.PI; // -> banyak jalan ke roma
+        // double keliling = 2 * jariJari * Math.PI;
+        double keliling = getDiameter() * Math.PI; // -> banyak jalan ke roma
         return keliling;
     }
 
     public double getLuas(){
         // double luas = jariJari * jariJari * Math.PI;
         double luas = Math.pow(jariJari, 2) * Math.PI;
-        return luas;
-        // return Math.round(luas); // -> kalo diperlukan pembulatan
+        // return luas;
+        return Math.round(luas); // -> kalo diperlukan pembulatan
     }
 
-    public void setJariJari(double newJariJari) {
-        jariJari = newJariJari;
-    }
+    // public void setJariJari(double newJariJari) {
+    //     jariJari = newJariJari;
+    // }
 
-    // Menampilkan informasi lingkaran
-    // Ex :
-    // Jari-jari       : 7.0
-    // Diameter        : 14.0
-    // Keliling        : 44.0
-    // Luas            : 154.0
-    public String toString() {
+    // // Menampilkan informasi lingkaran
+    // // Ex :
+    // // Jari-jari       : 7.0
+    // // Diameter        : 14.0
+    // // Keliling        : 44.0
+    // // Luas            : 154.0
+    // public String toString() {
+    //     String tampilan = String.format("Jari-jari\t: %f\nDiameter\t: %f\nKeliling\t: %f\nLuas\t\t: %f", jariJari, getDiameter(), getKeliling(), getLuas());
+    //     return tampilan;
+    // }
+
+    public String display() {
         String tampilan = String.format("Jari-jari\t: %f\nDiameter\t: %f\nKeliling\t: %f\nLuas\t\t: %f", jariJari, getDiameter(), getKeliling(), getLuas());
         return tampilan;
     }
--- src/materi/Main.java
+++ src/materi/Main.java
@@ -3,51 +3,54 @@ package materi;
 public class Main {
     public static void main(String[] args) {
         Lingkaran lingkaranMerah = new Lingkaran();
+        Lingkaran lingkaranKuning = new Lingkaran();
+        Lingkaran lingkaranHijau = new Lingkaran();
         
         // CARA 1
-        lingkaranMerah.jariJari = 7;
-        // lingkaranMerah.jariJari = 2; // -> penggunaan default value
+        // lingkaranMerah.jariJari = 7;
         
         // Diameter -> diameter = 2 * jari-jari
-        double diameter = 2 * lingkaranMerah.jariJari;
+        // double diameter = 2 * lingkaranMerah.jariJari;
 
         // Keliling -> keliling = 2 * jari-jari * PI
-        double keliling = 2 * lingkaranMerah.jariJari * Math.PI;
+        // double keliling = 2 * lingkaranMerah.jariJari * Math.PI;
 
         // Mengubah nilai jari-jari
         // lingkaranMerah.jariJari = 1;
 
         // Luas -> luas = jari-jari * jari-jari * PI
-        double luas = lingkaranMerah.jariJari * lingkaranMerah.jariJari * Math.PI;
+        // double luas = lingkaranMerah.jariJari * lingkaranMerah.jariJari * Math.PI;
 
-        System.out.println("Jari-jari\t: " + lingkaranMerah.jariJari);
-        System.out.println("Diameter\t: " + diameter);
-        System.out.println("Keliling\t: " + keliling);
-        System.out.println("Luas\t\t: " + luas);
-
-        /*
+        // System.out.println("Jari-jari\t: " + lingkaranMerah.jariJari);
+        // System.out.println("Diameter\t: " + diameter);
+        // System.out.println("Keliling\t: " + keliling);
+        // System.out.println("Luas\t\t: " + luas);
 
         // CARA 2 | langsung menggunakan default value
         // Diameter -> diameter = 2 * jari-jari
-        double diameter = lingkaranMerah.getDiameter();
+        // lingkaranMerah.jariJari = 2; // -> penggunaan default value
+      
+        // double diameter = lingkaranMerah.getDiameter();
 
-        // Keliling -> keliling = 2 * jari-jari * PI
-        double keliling = lingkaranMerah.getKeliling();
+        // // Keliling -> keliling = 2 * jari-jari * PI
+        // double keliling = lingkaranMerah.getKeliling();
 
         // Mengubah nilai jari-jari
         // lingkaranMerah.setJariJari(1);
 
         // Luas -> luas = jari-jari * jari-jari * PI
-        double luas = lingkaranMerah.getLuas();
+        // double luas = lingkaranMerah.getLuas();
 
-        System.out.println("Jari-jari\t: " + lingkaranMerah.jariJari);
-        System.out.println("Diameter\t: " + diameter);
-        System.out.println("Keliling\t: " + keliling);
-        System.out.println("Luas\t\t: " + luas);
+        // System.out.println("Jari-jari\t: " + lingkaranMerah.jariJari);
+        // System.out.println("Diameter\t: " + diameter);
+        // System.out.println("Keliling\t: " + keliling);
+        // System.out.println("Luas\t\t: " + luas);
 
         // CARA 3 | menampikan dengan toString()
-        System.out.println(lingkaranMerah.toString());
+        // Jika method toString dibuat maka output akan sesuai kode di methodnya
+        // Jika method tidak dibuat maka output berupa alamat object di memori
+        System.out.println(lingkaranMerah.display());
+        System.out.println(lingkaranMerah);
 
-        **/
     }
 }
