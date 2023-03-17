7a719a8abe15e01f362aa50cfcf882d84b17ec17
UrdhaEgha <eghaurdha@gmail.com>
2023-03-17 08:55:04 +0700

    Modul 2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,11 @@
 public class Main {
     public static void main(String[] args) throws Exception {
         
-        Peternakan peternakan = new Peternakan(25, 10, 7);
-        peternakan.Hewan1("Bebek");
-        peternakan.display1();
-        
-        peternakan.Hewan2("Domba");
-        peternakan.display2();
-        
-        peternakan.Hewan3("Unta");
-        peternakan.display3();
-        
-        peternakan.boostTernak();
-        System.out.println("\nHasil Ternak/Hari setelah Booster.");
-        System.out.println("1.) Bebek - Total Telur/Hari\t: " + peternakan.getJumlahTelur() + " Butir ");
-        System.out.println("2.) Domba - Total Bulu/Hari\t: " + peternakan.getJumlahBulu() + " kg ");
-        System.out.println("3.) Unta  - Total Susu/Hari\t: " + peternakan.getJumlahSusu() + " liter ");
+        Peternakan Bebek = new Peternakan("Bebek", 25, 2, 50);
+		Bebek.display1();
+        Peternakan Domba = new Peternakan("Domba", 10, 4,40);
+        Domba.display2();
+        Peternakan Unta = new Peternakan("Unta", 7, 5, 35 );
+	    Unta.display3();
     }
 }
--- src/Peternakan.java
+++ src/Peternakan.java
@@ -1,74 +1,60 @@
 public class Peternakan {
-    private int jumlahBebek;
-	 	private int jumlahDomba;
-	    private int jumlahUnta;
-	    
-	    private String jenisHewan;
-	    
-	    public Peternakan(int jumlahBebek, int jumlahDomba, int jumlahUnta) {
-	        this.jumlahBebek = jumlahBebek;
-	        this.jumlahUnta = jumlahUnta;
-	        this.jumlahDomba = jumlahDomba;
-	    }
-	    
-	    public void Hewan1(String jenisHewan) {
-	    	this.jenisHewan = jenisHewan;
-	    }
-	    
-	    public void Hewan2(String jenisHewan) {
-	    	this.jenisHewan = jenisHewan;
-	    }
-	    
-	    public void Hewan3(String jenisHewan) {
-	    	this.jenisHewan = jenisHewan;
-	    }
-	    public void boostTernak() {
-	    	// Mempercepat hasil ternak masing-masing hewan
-	        this.jumlahBebek *= 2.5;
-	        this.jumlahDomba *= 1.5;
-	        this.jumlahUnta *= 2;
-	    }
-	    
-	    public int getJumlahTelur() {
-	        // Menghitung jumlah telur yang dihasilkan semua bebek
-	        return jumlahBebek * 2;
-	    }
-	    
-	    public int getJumlahBulu() {
-	        // Menghitung jumlah bulu yang dihasilkan semua domba
-	        return jumlahDomba * 4;
-	    }
-	    
-	    public int getJumlahSusu() {
-	        // Menghitung jumlah susu yang dihasilkan semua unta
-	        return jumlahUnta * 5;
-	    }
-	    
-	    public void display1() {
-	    	System.out.println("Jenis\t\t\t: " + jenisHewan );
-	    	System.out.println("Jumlah\t\t\t: " + jumlahBebek +" ekor");
-	    	System.out.println("Telur/Ekor\t\t: " + "2 Butir");
-	    	System.out.println("Telur/Ekor\t\t: " + "5 Butir\t(Setelah Booster)");
-	    	System.out.println("Total Telur/Hari\t: " + getJumlahTelur() + " Butir");
+    private String jenisHewan;
+	   private int jumlahHewan;
+	   private int hasilPerEkor;
+	   private int hasilPerHari;
+	   
+	   public Peternakan (String jenisHewan, int jumlahHewan, int hasilPerEkor, int hasilPerHari){
+		   this.jenisHewan = jenisHewan;
+		   this.jumlahHewan = jumlahHewan;
+		   this.hasilPerEkor = hasilPerEkor;
+		   this.hasilPerHari = hasilPerHari;
+	   }
+	   
+	   public int getHasilTotBooster() {
+		   return jumlahHewan * hasilPerEkor;
+		   
+	   }
+	   public int getHasilperEkorSetelahBooster1() {
+		   return hasilPerEkor *= 2.5;
+	   }
+	   public int  getHasilperEkorSetelahBooster2() {
+		   return hasilPerEkor *= 1.5;
+	   }
+	   public int getHasilperEkorSetelahBooster3() {
+		   return hasilPerEkor *= 2;
+	   }
+	   
+	   public void display1() {
+		    System.out.println("Jenis\t\t\t: " + jenisHewan );
+	    	System.out.println("Jumlah\t\t\t: " + jumlahHewan +" ekor");
+	    	System.out.println("Telur/Ekor\t\t: " + hasilPerEkor);
+	    	System.out.println("Telur/Ekor\t\t: " + getHasilperEkorSetelahBooster1() + " Butir (Setelah Booster)");
+	    	System.out.println("Total Telur/Hari\t: " +  hasilPerHari+ " Butir");
+	    	System.out.println("Total Telur/Hari\t: " +  getHasilTotBooster() + " Butir (Setelah Booster)");
 	    	System.out.println("=====================================================");
-	    }
-	    
-	    public void display2() {
-	    	System.out.println("Jenis\t\t\t: " + jenisHewan );
-	    	System.out.println("Jumlah\t\t\t: " + jumlahDomba + " ekor");
-	    	System.out.println("Bulu/Ekor\t\t: " + "4 kg");
-	    	System.out.println("Bulu/Ekor\t\t: " + "6 kg\t\t(Setelah Booster)");
-	    	System.out.println("Total Bulu/Hari\t\t: " + getJumlahBulu() + " kg");
-	    	System.out.println("=====================================================");  
-	    }
-	    
-	    public void display3() {
-	    	System.out.println("Jenis\t\t\t: " + jenisHewan );
-	    	System.out.println("Jumlah\t\t\t: " + jumlahUnta + " ekor");
-	    	System.out.println("Bulu/Ekor\t\t: " + "5 liter");
-	    	System.out.println("Bulu/Ekor\t\t: " + "10 liter\t(Setelah Booster)");
-	    	System.out.println("Total Susu/Hari\t\t: " + getJumlahSusu() + " liter");
-	    	System.out.println("=====================================================");  
-	    }
+		   
+	   }
+	   
+	   public void display2() {
+		   System.out.println("Jenis\t\t\t: " + jenisHewan );
+		   System.out.println("Jumlah\t\t\t: " + jumlahHewan + " ekor");
+		   System.out.println("Bulu/Ekor\t\t: " + hasilPerEkor);
+		   System.out.println("Bulu/Ekor\t\t: " +  getHasilperEkorSetelahBooster2() + " kg (Setelah Booster)" );
+		   System.out.println("Total Bulu/Hari\t\t: " + + hasilPerHari + " kg");
+		   System.out.println("Total Bulu/Hari\t\t: " +  getHasilTotBooster() + " kg (Setelah Booster)");
+		   System.out.println("=====================================================");
+	   }
+	   
+	   public void display3() {
+		   System.out.println("Jenis\t\t\t: " + jenisHewan );
+		   System.out.println("Jumlah\t\t\t: " + jumlahHewan + " ekor");
+		   System.out.println("Susu/Ekor\t\t: " + hasilPerEkor);
+		   System.out.println("Susu/Ekor\t\t: " + getHasilperEkorSetelahBooster3() + " liter (Setelah Booster)");
+		   System.out.println("Total Susu/Hari\t\t: "  + hasilPerHari + " liter");
+		   System.out.println("Total Susu/Hari\t\t: " +  getHasilTotBooster() + " liter (Setelah Booster)");
+	       System.out.println("=====================================================");  
+	   }
+
         
 }
