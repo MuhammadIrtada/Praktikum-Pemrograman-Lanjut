18573575996545692f2fdbc18eeb2e0e45b5a981
Ghatfaner <ghatfaneryrazan@student.ub.ac.id>
2023-03-25 12:52:08 +0700

    Added main and Peternakan from Modul 3 & 4

---
+++ src/Modul_3/main.java
@@ -0,0 +1,24 @@
+package Praktikum.Modul3_4;
+
+import java.util.Scanner;
+
+import javax.annotation.processing.SupportedOptions;
+
+public class main {
+  public static void main(String[] args) {
+    String[] jenisHewan = { "Bebek", "Unta", "Domba" };
+    String[][] hewanRas = { { "Telur Biasa", "Telur Asin" }, { "Susu Putih", "Susu Coklat" },
+        { "Bulu Hitam", "Bulu Putih" } };
+    int[][] jumlahHewanRas = { { 15, 10 }, { 3, 4 }, { 4, 6 } };
+    double[][] hewanDosisHarian = new double[jumlahHewanRas.length][jumlahHewanRas[0].length];
+    double[][] hewanHasilDosisHarian = new double[jumlahHewanRas.length][jumlahHewanRas[0].length];
+
+    Peternakan hewanTernak = new Peternakan(jenisHewan, hewanRas, jumlahHewanRas, hewanDosisHarian,
+                                            hewanHasilDosisHarian);
+
+    Peternakan hewanTernak1 = new Peternakan(); // jika ada sebuah constructor tanpa parameter, maka output default akan
+                                                // ditampilkan
+
+    Peternakan.getInfo();
+  }
+}
