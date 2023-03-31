489c52f84b3883a32570ee44f7e4a8fbd174fcfe
IStoum <ahmadakrom563@gmail.com>
2023-03-31 17:16:21 +0700

    Ahmad Akrom kamaluddin - 225150700111005

---
+++ src/Modul5/Jenis.java
@@ -0,0 +1,17 @@
+package Modul5;
+
+public class Jenis {
+    private String[] jenis;
+
+    protected Jenis(String[] jenis) {
+        this.jenis = jenis;
+    }
+
+    protected String[] getJenis() {
+        return jenis;
+    }
+
+    protected String getJenisByIndex(int index) {
+        return jenis[index];
+    }
+}
