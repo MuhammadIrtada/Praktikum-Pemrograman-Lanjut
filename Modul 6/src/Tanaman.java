39324479d8853d82c1ba6efbbd3e15c9c29fd806
AfifahChairunnisa <afifah.chairunnisa.h@gmail.com>
2023-05-20 19:07:16 +0700

    Afifah Chairunnisa H - 225150707111018

---
+++ Modul 6/src/Tanaman.java
@@ -0,0 +1,37 @@
+public class Tanaman {
+    private String jenis;
+    private int kuantitas; // per tanaman (hektar)
+    private float hargaPerHektar;
+    private float perawatanPerHektar;
+
+    public Tanaman(String jenis, int kuantitas, float hargaPerHektar, float perawatanPerHektar) {
+        this.jenis = jenis;
+        this.kuantitas = kuantitas;
+        this.hargaPerHektar = hargaPerHektar;
+        this.perawatanPerHektar = perawatanPerHektar;
+    }
+
+    public String getJenis() {
+        return this.jenis;
+    }
+
+    public int getKuantitas() {
+        return this.kuantitas;
+    }
+
+    public float getHargaPerHektar() {
+        return this.hargaPerHektar;
+    }
+
+    public float getPerawatanPerHektar() {
+        return this.perawatanPerHektar;
+    }
+
+    public void addJumlah(int jumlahBaru){
+        this.kuantitas = this.kuantitas + jumlahBaru;
+    }
+
+    public String showInformasi() {
+        return String.format("Jenis\t\t\t: %s\nLuas\t\t\t: %d hektar\n", this.jenis, this.kuantitas);
+    }
+}
