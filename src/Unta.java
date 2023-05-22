e0ab0c0d7ab6b74dab151fbe382dc7c2c7eae330
RizaAthaya <111055458+RizaAthaya@users.noreply.github.com>
2023-05-22 12:10:25 +0700

    modul 7 selesai

---
+++ src/Unta.java
@@ -0,0 +1,31 @@
+
+public class Unta extends Hewan {
+    private int jumlah;
+    private int items;
+    private int harga;
+    private int rawat;
+
+    Unta(int jumlah, int items, int harga, int rawat) {
+        this.jumlah = jumlah;
+        this.items = items;
+        this.harga = harga;
+        this.rawat = rawat;
+        super.setJenis("Unta");
+        super.setTotalPendapatan(pendapatan());
+        super.setTotalPerawatan(perawatan());
+    }
+
+    @Override
+    public double pendapatan() {
+        double rumus = this.jumlah * this.harga * this.items * 30;
+        super.setPendapatan(rumus);
+        return rumus;
+    }
+
+    @Override
+    public double perawatan() {
+        double rumus = this.jumlah * this.rawat * 30;
+        super.setPerawatan(rumus);
+        return rumus;
+    }
+}
