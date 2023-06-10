50dee50b09fa585def3c8e4d885c3d7ac3627e75
Ghatfaner <ghatfaneryrazan@student.ub.ac.id>
2023-06-10 22:50:34 +0700

    Added Modul_6

---
+++ src/Modul_6/src/Duck.java
@@ -0,0 +1,29 @@
+public class Duck extends Animal{
+  private double dCostOfDuck = 110000;
+  private double dCostMaintenance = 3000;
+  private double dCostOfEgg;
+  private int intQuantityOfEgg;
+
+  Duck(String animalTypes, int quantity, int intQuantityOfEgg, double dCostOfEgg){
+    super(animalTypes, intQuantityOfEgg);
+    this.dCostOfEgg = dCostOfEgg;
+    this.intQuantityOfEgg = intQuantityOfEgg;
+  }
+
+  public double getPrice() {
+    return dCostOfDuck;
+  }
+
+  public double countTotalOfEgg() {
+    return this.intQuantityOfEgg * super.getQuantity();
+  }
+
+  public double countProfitOfEgg() {
+    return ((countTotalOfEgg() * this.dCostOfEgg) - (this.dCostMaintenance * this.getQuantity()));
+  }
+
+  public String displayInfo() {
+    String infoOfDuck = String.format("Telur/Ekor\t\t: %d Butir\nTotal Telur/Hari\t\t: %f Butir\nHarga Telur/Butir\t\t: Rp. %,.2f\nProfit/Hari\t\t: %,.2f", intQuantityOfEgg, countTotalOfEgg(), dCostOfEgg, countProfitOfEgg());
+    return super.displayInfo() + infoOfDuck;
+  }
+}
---
+++ src/Modul_6/src/Sheep.java
@@ -0,0 +1,29 @@
+public class Sheep extends Animal {
+  private double dCostOfSheep = 2450000;
+  private double dCostMaintenance = 45000;
+  private double dCostOfWool;
+  private int intQuantityOfWool;
+
+  Sheep(String animalTypes, int quantity, int intQuantityOfWool, double dCostOfWool){
+    super(animalTypes, intQuantityOfWool);
+    this.dCostOfWool = dCostOfWool;
+    this.intQuantityOfWool = intQuantityOfWool;
+  }
+
+  public double getPrice() {
+    return dCostOfSheep;
+  }
+
+  public double countTotalOfWool() {
+    return this.intQuantityOfWool * super.getQuantity();
+  }
+
+  public double countProfitOfWool() {
+    return ((countTotalOfWool() * this.dCostOfWool) - (this.dCostMaintenance * this.getQuantity()));
+  }
+
+  public String displayInfo() {
+    String infoOfSheep = String.format("Bulu/Ekor\t\t: %d Kg\nTotal Bulu/Hari\t\t: %f Kg\nHarga Bulu/Kg\t\t: Rp. %,.2f\nProfit/Hari\t\t: %,.2f", intQuantityOfWool, countTotalOfWool(), dCostOfWool, countProfitOfWool());
+    return super.displayInfo() + infoOfSheep;
+  }
+}
